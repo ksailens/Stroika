da5ece8916d1a5f2997a1b2ebe18cd1344c65962
ksailens <43430946+ksailens@users.noreply.github.com>
2019-05-19 13:27:00 +0300

    register

---
+++ application/controllers/controller_registration.php
@@ -0,0 +1,38 @@
+<?php
+
+class Controller_registration extends Controller
+{
+
+    function action_index()
+    {
+        $this->view->generate('registration.php', 'template_view.php');
+    }
+
+
+    function action_register()
+    {
+        if (isset($_SESSION['Login'])) {
+            header("Location: /user");
+        }
+        else{
+            header("Location: /index");
+        }
+    }
+
+
+    function action_logout(){
+        header("Location: /index");
+    }
+
+
+    function action_logined()
+    {
+        if (isset($_SESSION['Login'])) {
+            header("Location: /user");
+        }
+        else{
+            header("Location: /index");
+        }
+    }
+
+}
---
+++ application/views/registration.php
@@ -0,0 +1,19 @@
+<div class="blockForm">
+    <form action="/avtoriz/register"  onsubmit="return ValidRegister();" method="POST">
+        <li>
+            <br><h2 align=center>РЕГИСТРАЦИЯ</h2>
+            <div>
+                <br><br><b>Логин</b><br>
+                <input type="text" id="log_reg" name="log_reg" size="40"><br><br>
+                <b>Пароль</b><br>
+                <input type="text" id="pas_reg" name="pas_reg" size="40"><br><br>
+                <b>Email</b><br>
+                <input type="text" id="em_reg" name="em_reg" size="40"><br><br>
+                <b>Телефон</b><br>
+                <input type="text" id="tel_reg" name="tel_reg" size="40"><br><br>
+                <input class="inp" type="submit" value="ЗАРЕГИСТРИРОВАТЬСЯ">
+            </div>
+        </li>
+    </form>
+
+</div>
